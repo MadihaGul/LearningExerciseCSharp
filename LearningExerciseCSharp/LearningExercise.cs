5bc1b2aee8a12f0da549f48e6321bdfc43b545f0
madiha gul <mmadihagull@gmail.com>
2021-09-27 18:11:02 +0200

    Add project files.

---
+++ LearningExerciseCSharp/LearningExercise.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LearningExerciseCSharp
+{
+    public partial class Program
+    {
+        private static void RunExerciseThree()
+        {
+            Console.WriteLine("You sucessfully ran exercise three!");
+            Console.ResetColor();
+            Console.Write("Please Enter your first name: ");
+            string firstName = ChkUserinput(Console.ReadLine());
+            Console.Write("\nPlease Enter your last name: ");
+            string lastName = ChkUserinput(Console.ReadLine());
+            Console.WriteLine("\nYour name is " + firstName + " " + lastName + ".");
+
+        }
+        private static void RunExerciseFour()
+        {
+            String str1 = "";
+            //Output: “The brown fox jumped over the lazy dog”
+            String str = "The quick fox Jumped Over the DOG"; // String before string manipulation            
+            Console.WriteLine("You sucessfully ran exercise four!");
+            Console.ResetColor(); 
+            Console.WriteLine("Orignal string: "+str);
+            str1 = str;
+
+            str1=str1.Replace("DOG", "dog");
+            str1 = str1.Replace("J", "j");
+
+            str1 = str1.Remove(str1.IndexOf(" ")+1, 5);
+            str1 = str1.Insert(str1.IndexOf(" ")+1, "brown");
+
+            str1 = str1.Remove(str1.IndexOf("dog") - 9,1);
+            str1 = str1.Insert(str1.IndexOf("dog") - 8, "o");
+            str1 =str1.Insert(str1.IndexOf("dog"), "lazy ");
+                                   
+            Console.WriteLine("\nMamipulated String: "+str1);
+            //SubString
+        }
+
+        private static void RunExerciseFive()
+        {
+            Console.WriteLine("You sucessfully ran exercise five!");
+            Console.ResetColor(); 
+            String str = "Arrays are very common in programming, they look something like: [1,2,3,4,5]";
+            int position = str.IndexOf(":");
+
+            string str1 = str.Substring(position + 2);
+            str1=str1.Remove(str1.IndexOf("2"),4);
+            position = str1.IndexOf("5")+1;
+            for (int i = 6; i <= 10; i++)
+            {
+                str1 = str1.Insert(position, $",{i}");
+                position = position + 2;
+            }
+            Console.WriteLine("\nOrignal string: "+str+ "\nManipulated string: " + str1);
+        }
+    }
+}
---
+++ LearningExerciseCSharp/Main.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace LearningExerciseCSharp
+{
+    public partial class Program
+    {
+        static void Main(string[] args)
+        {
+            var keepAlive = true;
+            while (keepAlive)
+            {
+                try
+                {
+                    Console.WriteLine("Enter assignment number (or -1 to exit): ");
+                    var assignmentChoice = int.Parse(Console.ReadLine()??"");
+                    Console.ForegroundColor = ConsoleColor.Green;
+
+                    switch (assignmentChoice)
+                    {
+                        case 1:
+                            RunExerciseOne();break;
+                        case 2:
+                            RunExerciseTwo(); break;
+                        case 3:
+                            RunExerciseThree(); break;
+                        case 4:
+                            RunExerciseFour(); break;
+                        case 5:
+                            RunExerciseFive(); break;
+                        case -1: 
+                            keepAlive=false; break;
+                        default:
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("This is not a valid assignment number!");
+                            break;
+
+                    }
+                    Console.ResetColor();
+                    if (keepAlive)
+                    {
+                        Console.WriteLine("\nHit any key to continue!");
+                        Console.ReadKey();
+                    }
+                    Console.Clear();
+
+                }
+                catch
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("This is not a valid assignment number!");
+                    Console.ResetColor();
+                }
+            }
+            
+        }
+        private static void RunExerciseOne()
+        {
+            Console.WriteLine("You sucessfully ran exercise one!");
+            Console.ResetColor();
+            Console.WriteLine("Please Enter your first name: ");
+            string firstName = ChkUserinput(Console.ReadLine());
+            Console.WriteLine("Please Enter your last name: ");
+            string lastName = ChkUserinput(Console.ReadLine());
+            Console.WriteLine("Hello! "+firstName+" "+lastName+", I am glad to inform that you are the test subject of my first assignment ");
+        }
+        private static void RunExerciseTwo()
+        {
+            Console.WriteLine("You sucessfully ran exercise two!");
+            Console.ResetColor();
+            DateTime dateToday = DateTime.Now;
+            DateTime dateYesterday = dateToday.AddDays(-1);
+            DateTime dateTomorrow = dateToday.AddDays(1);
+
+            Console.WriteLine("Todays date is "+dateToday.ToShortDateString());
+            Console.WriteLine("Tomorrows date is "+dateTomorrow.ToShortDateString());
+            Console.WriteLine("Yesterdays date was "+dateYesterday.ToShortDateString());
+            Console.WriteLine("Timespan between yesterday and tomorrow is "+(dateTomorrow - dateYesterday).Days+" days");
+
+        }
+
+        public static string ChkUserinput(string Userinput)
+        {
+            while (Userinput == "")
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nInvalid! Try again.");
+                Userinput = Console.ReadLine();
+            }
+            Console.ResetColor();
+            return Userinput;
+        }
+    }
+}
